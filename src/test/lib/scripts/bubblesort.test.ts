fc488dc94011e23741197b948e4eb44d3b09b615
Daniel Nagel <danielnagel@mailbox.org>
2024-07-25 08:15:57 +0200

    added sort algorithm test

--- src/test/lib/scripts/bubblesort.test.ts
+++ src/test/lib/scripts/bubblesort.test.ts
@@ -81,8 +81,43 @@ describe('BubbleSort Script', () => {
 			expect(bubblesort.getGenerations()).toHaveLength(1);
 		})
 	});
-	test('return data', () => {
-		const bubblesort = new BubbleSort(sampleData);
-		expect(bubblesort.getData()).toStrictEqual(sampleData);
+	describe('test sort algorithm', () => {
+		test('throw error when there is no selection', () => {
+			const bubblesort = new BubbleSort([]);
+			expect(() => bubblesort.sortAlgorithm()).toThrowError('two selection indizes');
+		});
+		test('throw error when the selection indizes is bigger than the available data', () => {
+			const bubblesort = new BubbleSort([]);
+			bubblesort.setSelectionIndizes([0, 1])
+			expect(() => bubblesort.sortAlgorithm()).toThrowError('index exceeds data');
+		});
+		test('remove selection when sorted and selection is at the end of data', () => {
+			const bubblesort = new BubbleSort([...sortedSampleData]);
+			bubblesort.setSelectionIndizes([3, 4])
+			bubblesort.sortAlgorithm();
+			expect(bubblesort.getData()).toStrictEqual(sortedSampleData);
+			expect(bubblesort.getSelectionIndizes()).toStrictEqual([]);
+		});
+		test('set selection to the beginning, when the data is not sorted and the selection is at the end of data', () => {
+			const bubblesort = new BubbleSort([...sampleData]);
+			bubblesort.setSelectionIndizes([3, 4])
+			bubblesort.sortAlgorithm();
+			expect(bubblesort.getData()).toStrictEqual(sampleData);
+			expect(bubblesort.getSelectionIndizes()).toStrictEqual([0, 1]);
+		});
+		test('update selection, when current indizes are sorted', () => {
+			const bubblesort = new BubbleSort([...sampleData]);
+			bubblesort.setSelectionIndizes([0, 1])
+			bubblesort.sortAlgorithm();
+			expect(bubblesort.getData()).toStrictEqual(sampleData);
+			expect(bubblesort.getSelectionIndizes()).toStrictEqual([1, 2]);
+		});
+		test('update data, when current indizes are not sorted', () => {
+			const bubblesort = new BubbleSort([...sampleData]);
+			bubblesort.setSelectionIndizes([1, 2])
+			bubblesort.sortAlgorithm();
+			expect(bubblesort.getData()).toStrictEqual([2, 1, 5, 3, 4]);
+			expect(bubblesort.getSelectionIndizes()).toStrictEqual([1, 2]);
+		});
 	});
 });
