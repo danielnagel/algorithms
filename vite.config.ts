837ef83315b014d83dac517fb9ec62de5a125a6b
Daniel Nagel <danielnagel@mailbox.org>
2024-11-20 22:28:26 +0100

    dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import dtsPlugin from 'vite-plugin-dts'
 
 export default defineConfig({
+  publicDir: false,
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
