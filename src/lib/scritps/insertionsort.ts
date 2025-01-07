3e8f245d13606a95e08e056bfad4905ec20c215c
Daniel Nagel <danielnagel@mailbox.org>
2025-01-07 07:19:30 +0100

    refactored insertion sort

--- src/lib/scritps/insertionsort.ts
+++ src/lib/scritps/insertionsort.ts
@@ -21,43 +21,28 @@ export class InsertionSort extends SortScript {
 
 		const [firstIndex, lastIndex] = this.currentSelectionIndizes;
 
-		if (lastIndex > this.insertionIndex || firstIndex < 0)
+		if (lastIndex > this.insertionIndex)
 			throw Error('Selection index exceeds data size!');
 
 		const a = this.data[firstIndex];
 		const b = this.data[lastIndex];
 
 		if (a > b) {
-			// data needs to be sorted
+			// sort
 			this.data[firstIndex] = b;
 			this.data[lastIndex] = a;
-		} else if (firstIndex === 0) {
-			// "inner" loop is at the beginning
-			if (this.insertionIndex + 1 === this.data.length) {
-				// data is sorted
-				this.insertionIndex = 1;
-				this.currentSelectionIndizes = [];
-			} else {
-				// update insertion index
-				this.insertionIndex += 1;
-				this.insertionValue = this.data[this.insertionIndex];
-				this.currentSelectionIndizes = [this.insertionIndex - 1, this.insertionIndex];
-			}
-		} else if (firstIndex > 0) {
-			// "inner" loop has data left
-			if (this.data[firstIndex -1] > this.insertionValue) {
-				// next pair of data has to be sorted
-				this.currentSelectionIndizes = [firstIndex - 1, firstIndex];
-			} else if (this.insertionIndex + 1 === this.data.length) {
-				// data is sorted
-				this.insertionIndex = 1;
-				this.currentSelectionIndizes = [];
-			} else {
-				// no more data to sort in this iteration, update insertion index
-				this.insertionIndex += 1;
-				this.insertionValue = this.data[this.insertionIndex];
-				this.currentSelectionIndizes = [this.insertionIndex - 1, this.insertionIndex];
-			}
+		} else if (firstIndex > 0 && this.data[firstIndex -1] > this.insertionValue) {
+			// update seletion indizes
+			this.currentSelectionIndizes = [firstIndex - 1, firstIndex];
+		} else if (this.insertionIndex + 1 === this.data.length) {
+			// data is sorted
+			this.insertionIndex = 1;
+			this.currentSelectionIndizes = [];
+		} else {
+			// end of data, but not started, start from the beginning
+			this.insertionIndex += 1;
+			this.insertionValue = this.data[this.insertionIndex];
+			this.currentSelectionIndizes = [this.insertionIndex - 1, this.insertionIndex];
 		}
 	}
 
