3508143946007a00efaa5dfb2eb8538f2291d46d
Daniel Nagel <danielnagel@mailbox.org>
2025-01-08 06:03:42 +0100

    updated bubblesort

--- src/lib/scritps/bubblesort.ts
+++ src/lib/scritps/bubblesort.ts
@@ -25,33 +25,26 @@ export class BubbleSort extends SortScript {
 		const a = this.data[firstIndex];
 		const b = this.data[lastIndex];
 
-		// already correct, update selection indizes
-		if (a <= b) {
-			if (this.alreadySortedIndex <= 1) {
-				// is the data sorted? then remove selection
+		if (a > b) {
+			// sort data, but keep current selection indizes
+			this.data[firstIndex] = b;
+			this.data[lastIndex] = a;
+			this.switched = true;
+		} else if (lastIndex === this.alreadySortedIndex) {
+			// end of unsorted data and nothing switched, everything is already sorted
+			if (!this.switched || this.alreadySortedIndex <= 1) {
 				this.currentSelectionIndizes = [];
-			} else if (lastIndex === this.alreadySortedIndex) {
-				// end of unsorted data and nothing switched, everything is already sorted
-				if (!this.switched) {
-					this.currentSelectionIndizes = [];
-					this.alreadySortedIndex = 1;
-					return;
-				}
-				// reset selection to the begining, when its at the end
-				this.alreadySortedIndex = this.alreadySortedIndex - 1;
-				this.currentSelectionIndizes = [0, 1];
-				this.switched = false;
-			} else {
-				// last index is data.length when script is initialized
-				this.currentSelectionIndizes = [lastIndex, lastIndex + 1];
+				this.alreadySortedIndex = 1;
+				return;
 			}
-			return;
+			// reset selection to the begining, when its at the end
+			this.alreadySortedIndex = this.alreadySortedIndex - 1;
+			this.currentSelectionIndizes = [0, 1];
+			this.switched = false;
+		} else {
+			// last index is data.length when script is initialized
+			this.currentSelectionIndizes = [lastIndex, lastIndex + 1];
 		}
-
-		// sort data, but keep current selection indizes
-		this.data[firstIndex] = b;
-		this.data[lastIndex] = a;
-		this.switched = true;
 	}
 
 	sortData(data?: number[]) {
