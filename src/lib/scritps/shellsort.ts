e63815df0ba1fcd5d8824ce7e0bca6b9a7788c13
Daniel Nagel <danielnagel@mailbox.org>
2025-01-20 06:11:21 +0100

    refactored shellsort sortData

--- src/lib/scritps/shellsort.ts
+++ src/lib/scritps/shellsort.ts
@@ -8,70 +8,71 @@ import {
 export class ShellSort extends SortScript {
 
 	protected generations: ShellSortGeneration[] = [];
+
+	sortGapSequence(gap: number): void {
+		// selection list on main data
+		let selectionIndex = 0;
+		this.currentSelectionIndizes = [];
+		while (selectionIndex < this.data.length) {
+			this.currentSelectionIndizes.push(selectionIndex);
+			selectionIndex += gap;
+		}
+
+		while (this.currentSelectionIndizes.length > 0) {
+			// create sub list from selection list and main data
+			const subList = this.currentSelectionIndizes.map(i => this.data[i]);
+			const insertionSort = new InsertionSort(subList);
+			insertionSort.sortData();
+
+			// add subListSelection property to every insertion sort generation
+			// this way, its possible to see which entries in the sublist where selected
+			// and at which position the sublist was.
+			insertionSort.getGenerations().forEach(g => {
+				// ignore first and last generation
+				if (!g.selectionIndizes.length) return;
+
+				// update main data
+				const newData = [...this.data];
+				g.data.forEach((d, index) => newData[this.currentSelectionIndizes[index]] = d);
+
+				// create shell sort generation object, containing sub list selection property
+				this.generations.push({
+					selectionIndizes: this.currentSelectionIndizes,
+					data: newData,
+					subListSelection: g.selectionIndizes
+				});
+			});
+
+			this.currentSelectionIndizes = this.currentSelectionIndizes.map(s => s + 1);
+			if (this.currentSelectionIndizes[0] === gap) {
+				this.currentSelectionIndizes = [];
+			}
+			this.data = [...this.generations[this.generations.length - 1].data];
+		}
+	}
     
-	// TODO: refactor
 	sortData(data?: number[]): ShellSortGeneration[] {
 		if (this.data.length === 0 && (!data || data.length === 0))
 			throw Error('There is no data available!');
-
-		// always sort data
 		if (this.generations.length) this.generations = [];
 		if (data) this.data = [...data];
 
-		// initialize first generations
-		let minGapSequence = this.data.length;
-		const gapSequences: number[] = [];
-		while (minGapSequence > 1) {
-			minGapSequence = Math.round(minGapSequence / 2);
-			gapSequences.push(minGapSequence);
-		}
 		this.generations.push({
 			data: [...this.data],
 			selectionIndizes: [] 
 		});
 
-
-		gapSequences.slice(0,gapSequences.length - 1).forEach(gs => {
-			let selectionIndex = 0;
-			this.currentSelectionIndizes = [];
-			while (selectionIndex < this.data.length) {
-				this.currentSelectionIndizes.push(selectionIndex);
-				selectionIndex += gs;
-			}
-
-			while (this.currentSelectionIndizes.length > 0) {
-				// 1. neue Daten aaus den Indizes von currentSelectionIndizes zusammen bauen
-				const newDataList = this.currentSelectionIndizes.map(i => this.data[i]);
-				const insertionSort = new InsertionSort(newDataList);
-				insertionSort.sortData();
-
-				// TODO: 2. transform insertion sort generations from current selection to shell sort generations
-				insertionSort.getGenerations().forEach(g => {
-					if (!g.selectionIndizes.length) return;
-					const newData = [...this.data];
-					g.data.forEach((d, index) => {
-						newData[this.currentSelectionIndizes[index]] = d;
-					});
-					this.generations.push({
-						selectionIndizes: this.currentSelectionIndizes,
-						data: newData,
-						subListSelection: g.selectionIndizes
-					});
-				});
-
-
-				// 3. currentSelectionIndizes aktualisieren
-				this.currentSelectionIndizes = this.currentSelectionIndizes.map(s => s + 1);
-				if (this.currentSelectionIndizes[0] === gs) {
-					this.currentSelectionIndizes = [];
-				}
-
-				// 4. data aktualisieren
-				this.data = [...this.generations[this.generations.length - 1].data];
-			}
-		});
+		// Shellsort
+		const gapSequences: number[] = [];
+		let minGapSequence = this.data.length;
+		while (minGapSequence > 1) {
+			minGapSequence = Math.round(minGapSequence / 2);
+			gapSequences.push(minGapSequence);
+		}
+		gapSequences.slice(0,gapSequences.length - 1).forEach(gs => this.sortGapSequence(gs));
 		const insertionSort = new InsertionSort(this.data);
 		this.generations.push(...insertionSort.sortData().slice(1));
+
 		return this.generations;
 	}
 
