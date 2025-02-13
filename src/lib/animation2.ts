1efa16196756be39aa46d2bfcd723f561c0062f8
Daniel Nagel <danielnagel@mailbox.org>
2025-02-13 06:23:19 +0100

    randomize poc

--- src/lib/animation2.ts
+++ src/lib/animation2.ts
@@ -133,4 +133,11 @@ export const run = () => {
 			animationLoopState.animationFrameRequestId = requestAnimationFrame(() => mainLoop(animation, animationLoopState));
 		}
 	};
+
+    const randomizeButton = document.getElementById('randomize-button') as HTMLButtonElement;
+    if (!randomizeButton) throw Error('There is no randomize button in the DOM!');
+    randomizeButton.onclick = () => {
+        animationLoopState.generations = bs.addStateToGenerations(bs.sortData(generateRandomNumberArray(35, 100)));
+        animation.draw(animationLoopState);
+    }
 };
