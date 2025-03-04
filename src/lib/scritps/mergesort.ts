7a1bc22c32fc5ac63700ad6a2e9f6c6af200a47a
Daniel Nagel <danielnagel@mailbox.org>
2025-03-04 18:04:18 +0100

    lint

--- src/lib/scritps/mergesort.ts
+++ src/lib/scritps/mergesort.ts
@@ -5,21 +5,21 @@ import {
 export class MergeSort extends SortScript {
 
 	merge(l: number[], r: number[]): number[] {
-		const y: number[] = []
+		const y: number[] = [];
 		const nl = l.length;
 		const nr = r.length;
 		let il = 0;
-		for(let i = 0; i < nl+nr; i++) {
-			if(il > nl) {
-				y.push(r[i-il])
+		for (let i = 0; i < nl+nr; i++) {
+			if (il > nl) {
+				y.push(r[i-il]);
 				continue;
 			}
-			if(il <= i-nr) {
-				y.push(l[il])
+			if (il <= i-nr) {
+				y.push(l[il]);
 				il++;
 				continue;
 			}
-			if(l[il] <= r[i-il]) {
+			if (l[il] <= r[i-il]) {
 				y.push(l[il]);
 				il++;
 			} else {
@@ -29,16 +29,16 @@ export class MergeSort extends SortScript {
 		return y;
 	}
 
-    mergesort(data: number[]): number[] {
-		if(data.length <= 1) return data;
-		let l = data.slice(0, Math.floor(data.length/2))
-		let r = data.slice(Math.floor(data.length/2))
+	mergesort(data: number[]): number[] {
+		if (data.length <= 1) return data;
+		let l = data.slice(0, Math.floor(data.length/2));
+		let r = data.slice(Math.floor(data.length/2));
 		l = this.mergesort(l);
 		r = this.mergesort(r);
-        return this.merge(l, r);
-    }
+		return this.merge(l, r);
+	}
 
-    sortData(data?: number[]): Generation[] {
+	sortData(data?: number[]): Generation[] {
 		if (this.data.length === 0 && (!data || data.length === 0))
 			throw Error('There is no data available!');
 		if (this.generations.length) this.generations = [];
@@ -58,5 +58,5 @@ export class MergeSort extends SortScript {
 		});
 
 		return this.generations;
-    }
+	}
 }
